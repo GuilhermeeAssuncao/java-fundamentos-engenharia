2d733e0f346a1ad5dccaf7073f0c76e58f91eee0
GSA <guilhermeassuncao167@gmail.com>
2026-03-03 15:20:36 -0300

    dia 2

---
+++ src/DesafioLoops/ContadorInteligente.java
@@ -0,0 +1,21 @@
+package DesafioLoops;
+import java.util.Scanner;
+
+public class ContadorInteligente {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = 0;
+
+        for (i = 0; i <=100; i++){
+            if (i % 3 ==0 && i % 5==0){
+                System.out.println("FizzBuzz");
+            }else if  (i % 5 ==0){
+                System.out.println("Buzz");
+            } else if (i % 3 ==0 ) {
+                System.out.println("Fizz");
+            }else {
+                System.out.println(i);
+            }
+        }
+    }
+}
