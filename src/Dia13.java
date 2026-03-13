b112b03f4c674efc2c3abe1ab2cd91e2c8d9aaf6
GSA <guilhermeassuncao167@gmail.com>
2026-03-13 17:17:06 -0300

    dia 14

---
+++ src/Dia13.java
@@ -0,0 +1,52 @@
+public class Dia13 {
+
+        public static int segundoMaior(int[] n1){
+            int maior = Integer.MIN_VALUE;
+            int segundoMaior = Integer.MIN_VALUE;
+
+            for (int i = 0; i < n1.length; i++){
+                if (n1[i] > maior){
+                    segundoMaior = maior;
+                    maior = n1[i];
+                }else if (n1[i] > segundoMaior && n1[i] != maior){
+                    segundoMaior = n1[i];
+                }
+            }
+            return segundoMaior;
+        }
+
+        public static void inverterArray(int[] n2){
+            for  (int i = 0; i < n2.length / 2; i++){
+
+                    int temp = n2[i];
+                    n2[i] = n2[n2.length - 1 - i];
+                    n2[n2.length - 1 - i] = temp;
+            }
+            for (int j : n2) {
+                System.out.print(j + " ");
+            }
+
+        }
+
+        public static boolean ehPalindromo(int[] n3){
+
+                for (int i = 0; i < n3.length / 2; i++){
+
+                    if (n3[i] != n3[n3.length - 1 - i]){
+                        return false;
+                    }
+            }
+            return true;
+        }
+
+        public static void main(String[] args){
+            int[] n1 = {11,2,9,4,10};
+            int[] n2 = {1,2,3,4,5};
+            int[] n3 = {1,2,3,2,1};
+
+            System.out.println("o segundo maior numero é: " + segundoMaior(n1));
+            inverterArray(n2);
+            System.out.println(" ");
+            System.out.println("é um palindromo: " + ehPalindromo(n3));
+        }
+}
---
+++ src/Dia14.java
@@ -0,0 +1,64 @@
+import java.util.Scanner;
+
+public class Dia14 {
+    public static int buscaLinear(int[] n, int valor){
+        int numeroBuscado = -1;
+        for( int i = 0; i < n.length  ; i++){
+            if (n[i] == valor) {
+                numeroBuscado = i;
+                break;
+            }
+        }
+
+
+        return numeroBuscado;
+
+    }
+
+    public static int contarOcorrencias(int[] n2, int valor){
+        int contador = 0;
+        for( int i = 0; i< n2.length; i++){
+            if (n2[i] == valor){
+                contador++;
+            }
+        }
+        return contador;
+    }
+
+    public static int buscaBinaria(int[] n3, int valor){
+        int inicio = 0;
+        int fim = n3.length -1;
+
+        while (inicio <= fim){
+            int meio = (inicio + fim) /2;
+
+            if ( n3[meio] == valor){
+                return meio;
+            }
+            if (valor > n3[meio]){
+                inicio = meio + 1;
+            }
+            else {
+                fim = meio - 1;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        int [] n = {4,7,2,9,5};
+        int [] n2 = {2,5,2,8,2};
+        int [] n3 = {1,3,5,7,9,11};
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Digite o numero para busca: ");
+        int valor = sc.nextInt();
+        System.out.println("O valor esta em  " + buscaLinear(n, valor));
+        System.out.println("-------------------------------------------");
+        System.out.println("O " + valor + " apareceu " + contarOcorrencias(n2, valor) + " vezes.");
+        System.out.println("-------------------------------------------");
+        int posicao = buscaBinaria(n3, valor);
+
+        System.out.println("Encontrado no índice: " + posicao);
+    }
+}
