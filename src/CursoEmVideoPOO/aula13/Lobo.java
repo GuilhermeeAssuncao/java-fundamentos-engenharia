36e41e52eee48df151f1920f9495f86ba78479db
GSA <guilhermeassuncao167@gmail.com>
2026-04-29 13:12:30 -0300

    fim da aula 13 de POO

---
+++ src/CursoEmVideoPOO/aula13/Lobo.java
@@ -0,0 +1,8 @@
+package CursoEmVideoPOO.aula13;
+
+public class Lobo extends Mamifero{
+    @Override
+    public void emitirSom() {
+        System.out.println("AUUUUUUUU!");
+    }
+}
---
+++ src/CursoEmVideoPOO/aula13/main.java
@@ -0,0 +1,10 @@
+package CursoEmVideoPOO.aula13;
+
+public class main {
+    public static void main(String[] args) {
+        Cachorro c = new Cachorro();
+
+        c.reagir(2, 12.5f);
+        c.reagir(17, 4.5f);
+    }
+}
